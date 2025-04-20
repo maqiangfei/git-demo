28a639b3ee8ac52c46057b788fe8fc942a67111e
maqiangfei <maqiangfei@krunning.cn>
2025-04-20 14:06:58 +0800

    V1第二次修改

---
+++ src/main/java/com/qh/gitdemo/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.qh.gitdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author maqiangfei
+ * @since 2025/4/20 13:51
+ **/
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
