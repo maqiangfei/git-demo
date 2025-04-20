4f8983fefba6d946c1487d58aee7984514a3efa5
maqiangfei <maqiangfei@krunning.cn>
2025-04-20 13:14:55 +0800

    第一次提交

---
+++ src/main/java/com/qh/Main.java
@@ -0,0 +1,11 @@
+package com.qh;
+
+/**
+ * @author maqiangfei
+ * @since 2025/4/20 13:03
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
