47a63b86c1fb3993c6f60aa8b318d846ee771800
maqiangfei <maqiangfei@krunning.cn>
2025-04-20 13:49:35 +0800

    V1第一次修改

---
+++ src/main/java/com/qh/gitdemo/GitDemoApplication.java
@@ -0,0 +1,16 @@
+package com.qh.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author maqiangfei
+ * @since 2025/4/20 13:44
+ **/
+@SpringBootApplication
+public class GitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemoApplication.class, args);
+    }
+}
